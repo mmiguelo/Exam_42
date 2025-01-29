5809d2fc7aba44b68ec43779d198b693fe802f74
mmiguelo <marco_miguelote@hotmail.com>
2025-01-29 13:16:05 +0000

    reduced lines for lvl0 search_and_replace

--- level_0/search_and_replace.c
+++ level_0/search_and_replace.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
-int	main(int ac, char **av) - search_and_replace
+int	main(int ac, char **av)
 {
-	int i;
+	int i = 0;
 	if (ac == 4 && !av[2][1] && !av[3][1])
 	{
-		i = 0;
 		while(av[1][i])
 		{
 			if (av[1][i] == av[2][0])
