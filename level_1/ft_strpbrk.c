4580dd572b5ac7d3b98a51fd7784bc6e57e66b02
mmiguelo <marco_miguelote@hotmail.com>
2025-01-29 13:26:50 +0000

    reduced lines for lvl1 ft_strpbrk

--- level_1/ft_strpbrk.c
+++ level_1/ft_strpbrk.c
@@ -2,21 +2,18 @@
 
 char	*ft_strpbrk(const char *s1, const char *s2)
 {
-	int i;
+	int i = 0;
 	int j;
 	
 	if(!s1 || !s2)
 		return (0);
 	while (s1[i])
 	{
-		i = 0;
+		j = 0;
 		while (s2[j])
 		{
-			j = 0;
 			if (s1[i] == s2[j])
-			{
 				return((char *)&s1[i]);
-			}
 			j++;
 		}
 		i++;
