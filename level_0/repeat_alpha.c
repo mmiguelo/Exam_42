cbf207506f8252aaf587c05ebec745d9a05bb501
mmiguelo <marco_miguelote@hotmail.com>
2025-01-29 13:09:05 +0000

    reduced lines for lvl0 repeat_alpha

--- level_0/repeat_alpha.c
+++ level_0/repeat_alpha.c
@@ -1,26 +1,20 @@
 #include <unistd.h>
 
-int	main(int argc, char **argv) - repeat_alpha
+int	main(int argc, char **argv)
 {
-	int i;
-	int count;
+	int i = 0;
+	int count = 1;
 
 	if (argc == 2)
 	{
-		i = 0;
 		while (argv[1][i])
 		{
 			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
 				count = argv[1][i] - 'a' + 1;
-			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
 				count = argv[1][i] - 'A' + 1;
-			else
-				count = 1;
-			while (count)
-			{
+			while (count--)
 				write(1, &argv[1][i], 1);
-				count --;
-			}
 			i++;
 		}
 	}
