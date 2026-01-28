3a5a3e072539de44de18565c92fccdf6473ad9d4
root <root@DESKTOP-V4EO9N9.localdomain>
2026-01-28 15:27:58 +0000

    added solution to polyset exercise

---
+++ Rank_5/level1/polyset/set.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "subject/searchable_bag.hpp"
+
+class set {
+	private:
+		searchable_bag& _bag;
+	public:
+		set(searchable_bag& bag);
+		~set();
+		set(const set& other);
+		set&operator=(const set& other);
+
+		void insert(int);
+		void insert(int *, int);
+		void print() const;
+		void clear();
+		bool has(int) const;
+
+		const searchable_bag& get_bag() const;
+};
