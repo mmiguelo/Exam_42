915b53f37cbec078bf6639735d966fe73f5b0883
mmiguelo <marco_miguelote@hotmail.com>
2026-03-07 16:43:49 +0000

    updated solution and subject

--- Rank_5/level2/bsq/bsq.c
+++ Rank_5/level2/bsq/bsq.c
@@ -1,137 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
-typedef struct s_bsq {
-	int lines;
-	int width;
-	char empt;
-	char obs;
-	char full;
-	char **map;
-} t_bsq;
-
-int ft_strlen(char *str) {
-	int size = 0;
-	for (int i = 0; i < str[i]; i++)
-		size++;
-	return size;
-}
+typedef struct {
+	int lines, width;
+	char empt, obs, full;
+	char** map;
+} BSQ;
 
 int min(int a, int b, int c) {
-	if (a < b && a < c)
-		return a;
-	if (b < c)
-		return b;
-	return c;
+	int m = a;
+	if (b < m)
+		m = b;
+	if (c < m)
+		m = c;
+	return (m);
 }
 
-void free_map(t_bsq *bsq) {
-	if (bsq->map) {
-		for (int i = 0; i < bsq->lines; i++)
-			free(bsq->map[i]);
-		free(bsq->map);
-	}
+void free_map(BSQ *b) {
+	for (int i = 0; i < b->lines; i++)
+		free(b->map[i]);
+	free(b->map);
 }
 
-void print_map(t_bsq *bsq) {
-	for (int i = 0; i < bsq->lines; i++)
-		printf("%s\n", bsq->map[i]);
-}
-
-void solve_bsq(t_bsq *bsq) {
-	int board[bsq->lines][bsq->width];
-	int max = 0, max_x = 0, max_y = 0; 
-	
-	for (int i = 0; i < bsq->lines; i++) {
-		for (int j = 0; j < bsq->width; j++) {
-			if (bsq-> map[i][j] == bsq->obs)
-				board[i][j] == 0;
+void solve_bsq(BSQ *b) {
+	int **board = malloc(b->lines * sizeof(int*));
+	int max = 0, mx = 0, my = 0;
+	for (int i = 0; i < b->lines; i++) {
+		board[i] = calloc(b->width, sizeof(int));
+		for (int j = 0; j < b->width; j++) {
+			if (b->map[i][j] == b->obs)
+				board[i][j] = 0;
 			else if (i == 0 || j == 0)
-				board[i][j] == 1;
+				board[i][j] = 1;
 			else
-				board[i][j] = min(board[i - 1][j], board[i][j - 1], board[i - 1][j - 1]) + 1;
-			if (board[i][j] > max) {
+				board[i][j] = min(board[i-1][j], board[i][j-1], board[i-1][j-1]) + 1;
+			if(board[i][j] > max) {
 				max = board[i][j];
-				max_x = j;
-				max_y = i;
+				mx = j;
+				my = i;
 			}
 		}
 	}
-
-	int start_x = max_x - max + 1;
-	int start_y = max_y - max + 1;
-	int end_x = start_x + max;
-	int end_y = start_y + max;
-
-	for (int i = start_y; i < end_y; i++)
-		for (int j = start_x; j < end_x; j++)
-			bsq->map[i][j] = bsq->full;
+	for (int i = my - max + 1; i <= my; i++) 
+		for (int j = mx - max + 1; j <= mx; j++)
+			b->map[i][j] = b->full;
+	for (int i = 0; i < b->lines; i++)
+		free(board[i]);
+	free(board);
 }
 
-int check_chars(t_bsq *bsq, char *line) {
-	for (int i = 0; line[i]; i++)
-		if (line[i] != bsq->empt && line[i] != bsq->obs)
-			return 0;
-	return 1;
-}
-
-int parse_map(t_bsq *bsq, FILE *file) {
-	bsq->map = malloc(sizeof(char *) * bsq->lines);
-	if (!bsq->map)
+int parse(BSQ *b, FILE *f) {
+	int ret = fscanf(f, "%d %c %c %c\n", &b->lines, &b->empt, &b->obs, &b->full);
+	if (ret != 4 || b->lines <= 0)
 		return 0;
-	for (int i = 0; i < bsq->lines; i++) {
-		char *line = NULL;
-		size_t s = 0;
-		if (getline(&line, &s, file) == 1)
-			return (free(line), 0);
-		int len = ft_strlen(line);
-		if (line[len -1] == '\n') {
-			line[len - 1] = '\0';
-			len --;
+	if (b->empt == b->obs || b->empt == b->full || b->obs == b->full)
+		return 0;
+	b->map = malloc(b->lines * sizeof(char*));
+	for (int i = 0; i < b->lines; i++) {
+		char* line = NULL;
+		size_t cap = 0;
+		ssize_t len = getline(&line, &cap, f);
+		if (len <= 0) {
+			free(line);
+			b->lines = i;
+			free_map(b);
+			return 0;
 		}
+		if (line[len - 1] == '\n')
+			line[--len] = '\0';
 		if (i == 0)
-			bsq->width = len;
-		else if (bsq->width != len)
-			return (free(line), 0);
-		if (!check_chars(bsq, line))
-			return (free(line), 0);
-		bsq->map[i] = line;
+			b->width = len;
+		else if(len != b->width) {
+			free(line);
+			b->lines = i;
+			free_map(b);
+			return 0;
+		}
+		b->map[i] = line;
 	}
 	return 1;
 }
 
-int parse_header(t_bsq *bsq, FILE *file) {
-	if (fscanf(file, "%i%c%c%c\n", &bsq->lines, &bsq->empt, &bsq->obs, &bsq->full) != 4)
-		return 0;
-	if (bsq->lines <= 0)
-		return 0;
-	if (bsq->empt == bsq->obs || bsq->empt == bsq->full || bsq->obs == bsq->full)
-		return 0;
-	return 1;
-}
-
-void start(FILE *file) {
-	t_bsq bsq;
-
-	if(!parse_header(&bsq, file) || !parse_map(&bsq, file))
-		return (printf("Error: invalid map\n"), free_map(&bsq));
-	solve_bsq(&bsq);
-	print_map(&bsq);
-	free_map(&bsq);
+void start(FILE *f) {
+	BSQ b = {0};
+	if (!parse(&b, f))
+		return (void)fprintf(stderr, "Error: invalid map\n");
+	solve_bsq(&b);
+	for (int i = 0; i < b.lines; i++)
+		puts(b.map[i]);
+	free_map(&b);
 }
 
 int main(int ac, char **av) {
-	if (ac == 1)
-		start(stdin);
-	else if (ac == 2) {
-		FILE *file = fopen(av[1], "r");
-		if (!file)
-			return (printf("Error: opening file failed\n"), 1);
-		start(file);
-		fclose(file);
-	}
+	FILE *f;
+	if (ac == 2)
+		f = fopen(av[1], "r");
 	else
-		return (printf("Error: wrong number of args\n"), 1);
+		f = stdin;
+	if (!f)
+		return (fprintf(stderr, "Error opening the file\n"), 1);
+	start(f);
+	if (ac == 2)
+		fclose(f);
 	return 0;
 }
